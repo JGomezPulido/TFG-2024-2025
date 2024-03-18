a709777165c5cbb506d182c81a0e704a537b681e
jgomez18UCM <81363820+jgomez18UCM@users.noreply.github.com>
2024-03-18 22:34:50 +0100

    Creada solución y dockerfile para los test
    He puesto en el README.md un enlace con un pequeño tutorial de como lanzar la imagen y conectarla a Visual Studio

---
+++ LocalizationTests/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main(void) {
+	std::cout << "Hello Linux!" << std::endl;
+	std::string c;
+	std::cin >> c;
+	std::cout << c << std::endl;
+	return 0;
+}

f0b46649b3f737b06bf2312ca7402a20f7025151
Dewei Chen <deweiche@ucm.es>
2024-07-22 16:31:35 +0200

    main anterior

--- LocalizationTests/src/LocalizationTestsApp/main.cpp
+++ LocalizationTests/src/LocalizationTestsApp/main.cpp
@@ -1,11 +1,21 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include "Tesseract.h"
+#include "TestsAssert.h"
+#include "BadlyImplemented.h"
 int main(void) {
-	Tesseract tess = Tesseract();
-	
-	//if (tess.trainModel("spa", "Courier Prime", 1000)) {
-	//	std::cout << "YES\n";
-	//}else std::cout << "NO\n";
-	tess.getText("/home/images", "Courier Prime");
+	PlaceholderVector placeholders = PlaceholderVector();
+	placeholders.push_back({ '{','}' });
+	BadlyImplemented tests = BadlyImplemented(placeholders);
+	tests.test("Hola de buenas");
+	tests.test("Hola {name}, que tal est�s {laugh}");
+	tests.test("Hola {name{player1}}");
+	return 0;
+
+	//Tesseract tess = Tesseract();
+	//
+	////if (tess.trainModel("spa", "Courier Prime", 1000)) {
+	////	std::cout << "YES\n";
+	////}else std::cout << "NO\n";
+	//tess.getText("/home/images", "Courier Prime");
 }

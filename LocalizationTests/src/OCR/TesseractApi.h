f99207e028405e8b4b845c5fb64711b52efcbf99
Dewei Chen <deweiche@ucm.es>
2025-02-01 12:39:35 +0100

    Sistema de logs simple

--- LocalizationTests/src/OCR/TesseractApi.h
+++ LocalizationTests/src/OCR/TesseractApi.h
@@ -1,13 +1,13 @@
-#pragma once
-#ifndef TESSERACTAPI_H
-#define TESSERACTAPI_H
-#include "OCR.h"
-class TesseractApi : OCR
-{
-public:
-	void test();
-private:
-
-};
-
-#endif
+//#pragma once
+//#ifndef TESSERACTAPI_H
+//#define TESSERACTAPI_H
+//#include "OCR.h"
+//class TesseractApi : OCR
+//{
+//public:
+//	void test();
+//private:
+//
+//};
+//
+//#endif
